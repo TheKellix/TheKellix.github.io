868fb287f486f33846a506b4252a93a21979aa68
TheKellix <popovsemen562@gmail.com>
2026-01-10 00:33:10 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $('.carousel').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    prevArrow:'<button type="button" class="slick-prev"></button>',
+    nextArrow:'<button type="button" class="slick-next"></button>',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
